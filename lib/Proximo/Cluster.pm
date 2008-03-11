45b418db4f329700a63e7df408c8bca05ed060d9
Mark Smith <mark@xb95.com>
2008-03-11 08:02:45 +0000

    * some more cluster work, still unstable

---
+++ lib/Proximo/Cluster.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#
+# the implementation of the basic Cluster class, mostly this is just defining the
+# interface that the rest of the classes that implement this have to adhere to.
+
+package Proximo::Cluster;
+
+use strict;
+
+use fields (
+        'name',    # name of this cluster
+    );
+    
+# constructs a new cluster for people to use.  only one of these should be created
+# per cluster defined, not one per connection (see the Instance classes)
+sub new {
+    my Proximo::Cluster $self = $_[0];
+    $self = fields::new( $self ) unless ref $self;
+
+    # initialization
+    $self->{name} = $_[1];
+
+    return $self;
+}
+
+# constructs an instance of this particular cluster and returns it, must be
+# implemented by the descendant classes
+sub instance {
+    Proximo::fatal( 'Proximo::Cluster::instance() not implemented!' );
+}
+
+# called by the management console to set various attributes
+sub set {
+    my Proximo::Cluster $self = $_[0];
+    my ( $key, $val ) = ( $_[1], $_[2] );
+
+    $key = lc $key;
+
+    # we actually don't have anything we set right now, but
+    # it's possible we will in the future, so we expect the subclasses
+    # to call up to us...
+    return 1;
+}
+
+1;
--- lib/Proximo/MySQL/Cluster.pm
+++ lib/Proximo/MySQL/Cluster.pm
@@ -10,11 +10,14 @@
 package Proximo::MySQL::Cluster;
 
 use strict;
+use Proximo::Cluster;
+use base 'Proximo::Cluster';
 
 use fields (
-        'name',    # name of this cluster
-        'setup',   # how this cluster is configured
-        'masters', # array of nodes designated
+        'setup',     # how this cluster is configured
+        'masters',   # array of nodes designated as masters
+        'slaves',    # array of nodes designated as slaves
+        'readonly',  # if enabled, do no allow writes
     );
 
 # constructs a new cluster for people to use.  only one of these should be created
@@ -23,8 +26,14 @@ sub new {
     my Proximo::MySQL::Cluster $self = $_[0];
     $self = fields::new( $self ) unless ref $self;
 
-    # initialization
-    $self->{name} = $_[1];
+    # init parent
+    $self->SUPER::new( @_ );
+
+    # self variables
+    $self->{setup}    = 'single';
+    $self->{masters}  = [];
+    $self->{slaves}   = [];
+    $self->{readonly} = 0;
 
     return $self;
 }
@@ -43,11 +52,87 @@ sub set {
     my ( $key, $val ) = ( $_[1], $_[2] );
 
     $key = lc $key;
+    my $truefalse = sub {
+        my $val = shift;
+        return 1 if $val =~ /^(?:true|on|yes|1|affirmative)$/;
+        return 0 if $val =~ /^(?:false|off|no|0|negative)$/;
+        return undef;
+    };
 
-    if ( $key eq 'name' ) {
-        $self->{name} = $val;
+    # set list of masters/slaves
+    if ( $key eq 'masters' || $key eq 'slaves' ) {
+        my @new;
+        my @ips = split /\s*,\s*/, $val;
+        foreach my $ip ( @ips ) {
+            return Proximo::warn( 'IP %s not valid in set of %s.', $ip, $key )
+                unless $ip =~ /^\d+\.\d+\.\d+\.\d+(?::\d+)?$/;
+            $ip = "$ip:3306" unless $ip =~ /:/;
+            push @new, $ip;
+        }
+        
+        # looks like they're valid, ok, let's replace our list with them...
+        $self->{$key} = \@new;
+        return 1;
+
+    # maybe they want to change the cluster setup?
+    } elsif ( $key eq 'setup' ) {
+        return Proximo::warn( 'Invalid setup %s for MySQL cluster.', $key )
+            unless $val =~ /^(?:single|master-slave|master-master)$/;
+    
+        # save it
+        $self->{setup} = $val;    
+        return 1;
+
+    # turn readonly on/off
+    } elsif ( $key eq 'readonly' ) {
+        my $rv = $truefalse->( $val );
+        return Proximo::warn( 'Invalid value %s for readonly.', $key )
+            unless $rv;
+
+        # save it
+        $self->{readonly} = $rv;
+        return 1;
 
     }
+
+    # fall through to parent
+    return $self->SUPER::set( $key, $val );
 }
+
+# returns the arrayref of masters
+sub masters {
+    my Proximo::MySQL::Cluster $self = $_[0];
+    return $self->{masters};
+}
+
+# how many masters we have, utility function
+sub master_count {
+    my Proximo::MySQL::Cluster $self = $_[0];
+    return scalar( @{ $self->{masters} } );
+}
+
+# arrayref of slaves
+sub slaves {
+    my Proximo::MySQL::Cluster $self = $_[0];
+    return $self->{slaves};
+}
+
+# how many slaves we have, utility function
+sub slave_count {
+    my Proximo::MySQL::Cluster $self = $_[0];
+    return scalar( @{ $self->{slaves} } );
+}
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+package Proximo::MySQL::Cluster::Instance;
+
+use strict;
+use Proximo::MySQL::Cluster;
+use base 'Proximo::MySQL::Cluster';
+
+
 
 1;
